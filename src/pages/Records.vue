7a5bfaa0040d249783442e0f82c30d6e05b59c13
あさぴっぴ1020 <88693257+Asapi1020@users.noreply.github.com>
2025-02-09 12:15:36 +0900

    ✨ 読み込み中表示を追加し、DateをISO形式に対応

--- src/pages/Records.vue
+++ src/pages/Records.vue
@@ -9,10 +9,11 @@ const route = useRoute();
 const router = useRouter();
 
 const PER_PAGE = 20;
-const page = computed(() => (route.query.page ? Number(route.query.page) : 0));
+const page = computed(() => (route.query.page ? Number(route.query.page) : 1));
 
 const records = ref<Record[]>([]);
 const totalRecordsNum = ref<number>(0);
+const isLoading = ref<boolean>(false);
 
 const onPageChange = (newPage: number) => {
 	router.push({query: {...route.query, page: newPage}});
@@ -22,17 +23,15 @@ const navigateTo = (path: string) => {
 	router.push(path);
 };
 
-const gotoNextPage = () => {
-	records.value = [];
-	navigateTo(`/records?page=${page.value + 1}`);
-};
-
 const getRecords = async () => {
+	isLoading.value = true;
 	try {
 		const fetchedRecords = await apiClient.getRecords(page.value);
 		[records.value, totalRecordsNum.value] = fetchedRecords;
-	}catch(error){
+	} catch(error) {
 		console.error(error);
+	} finally {
+		isLoading.value = false;
 	}
 }
 
@@ -43,44 +42,51 @@ watch(() => route.query.page, getRecords);
 <template>
 	<v-main style="min-height: 300px; margin-left: 20px; margin-top: 20px; margin-right: 20px">
 		<div class="table-container mx-4">
-			<table>
-				<thead>
-					<tr>
-						<th>Date</th>
-						<th>Server</th>
-						<th>Result</th>
-						<th>Map</th>
-						<th>SpawnCycle</th>
-						<th>MaxMonsters</th>
-						<th>CohortSize</th>
-						<th>SpawnPoll</th>
-						<th>WaveSizeFakes</th>
-						<th>Players</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr v-for="record in records" :key="record.id">
-						<td>{{ record.matchInfo.timeStamp.split(" - ")[0] }}</td>
-						<td>{{ record.matchInfo.isSolo ? "Solo" : record.matchInfo.serverName ?? record.matchInfo.serverIP }}</td>
-						<td>{{ record.matchInfo.isVictory ? "Win" : `Defeat @${record.matchInfo.defeatWave}` }}</td>
-						<td>{{ record.matchInfo.mapName }}</td>
-						<td>{{ record.matchInfo.CDInfo.spawnCycle }}</td>
-						<td>{{ record.matchInfo.CDInfo.maxMonsters }}</td>
-						<td>{{ record.matchInfo.CDInfo.cohortSize }}</td>
-						<td>{{ record.matchInfo.CDInfo.spawnPoll }}</td>
-						<td>{{ record.matchInfo.CDInfo.waveSizeFakes }}</td>
-						<td>
-							<span v-for="(stat, index) in record.userStats" :key="index">
-								{{ stat.playerName ?? stat.steamID }}
-								<span v-if="index < record.userStats.length - 1">, </span>
-							</span>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-		</div>
-
-		<v-pagination v-model="page" :length="Math.ceil(totalRecordsNum / PER_PAGE)" class="text-center mt-4" :totalVisible="9" @update:modelValue="onPageChange"></v-pagination>
+			<div v-if="isLoading" class="d-flex justify-center my-4">
+                <v-progress-circular
+                    indeterminate
+                    color="primary"
+                ></v-progress-circular>
+            </div>
+			<div v-else>
+				<table>
+					<thead>
+						<tr>
+							<th>Date</th>
+							<th>Server</th>
+							<th>Result</th>
+							<th>Map</th>
+							<th>SpawnCycle</th>
+							<th>MaxMonsters</th>
+							<th>CohortSize</th>
+							<th>SpawnPoll</th>
+							<th>WaveSizeFakes</th>
+							<th>Players</th>
+						</tr>
+					</thead>
+					<tbody>
+						<tr v-for="record in records" :key="record.id">
+							<td>{{ new Date(record.matchInfo.timeStamp).toLocaleDateString() }}</td>
+							<td>{{ record.matchInfo.isSolo ? "Solo" : record.matchInfo.serverName ?? record.matchInfo.serverIP }}</td>
+							<td>{{ record.matchInfo.isVictory ? "Win" : `Defeat @${record.matchInfo.defeatWave}` }}</td>
+							<td>{{ record.matchInfo.mapName }}</td>
+							<td>{{ record.matchInfo.CDInfo.spawnCycle }}</td>
+							<td>{{ record.matchInfo.CDInfo.maxMonsters }}</td>
+							<td>{{ record.matchInfo.CDInfo.cohortSize }}</td>
+							<td>{{ record.matchInfo.CDInfo.spawnPoll }}</td>
+							<td>{{ record.matchInfo.CDInfo.waveSizeFakes }}</td>
+							<td>
+								<span v-for="(stat, index) in record.userStats" :key="index">
+									{{ stat.playerName ?? stat.steamID }}
+									<span v-if="index < record.userStats.length - 1">, </span>
+								</span>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+				<v-pagination v-model="page" :length="Math.ceil(totalRecordsNum / PER_PAGE)" class="text-center mt-4" :totalVisible="9" @update:modelValue="onPageChange"></v-pagination>
+			</div>
+		</div>		
 	</v-main>
 </template>
 
