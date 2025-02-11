7d280c62fadbafee28c820fa212b925621662656
あさぴっぴ1020 <88693257+Asapi1020@users.noreply.github.com>
2025-02-12 08:35:15 +0900

    ✨ headerのタイトルを記録一覧へのリンクに変更

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,25 @@
 <script setup lang="ts"></script>
 
 <template>
-	<v-app-bar title="CD Record Leader Board"></v-app-bar>
+	<v-app-bar>
+		<template v-slot:prepend>
+			<router-link to="/records" class="app-bar-title">
+				CD Record Leader Board
+			</router-link>
+		</template>
+	</v-app-bar>
 </template>
 
 <style scoped lang="scss">
 .v-app-bar {
 	padding: 4px;
 	border-bottom: 2px solid #555;
+}
+
+.app-bar-title {
+	font-size: 1.5rem;
+	color: #ffffff;
+	text-decoration: none;
+	margin-left: 12px;
 }
 </style>
